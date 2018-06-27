d33aa72889e71da00cb8c100988795a11866e0ca
Hassan Algoz <HassanAlgoz@gmail.com>
2018-06-27 12:56:35 +0300

    Show UI elements after login to room (namespace).

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,7 @@
 import socket from './net.js'
 
-// Broadcast a message to connected users when someone connects or disconnects
-// Add “{user} is typing” functionality
-// Show who’s online
-// Add private messaging
-// Share your improvements!
+// username is used to be compared with 'from' in 'msg' events
+let username;
 
 window.addEventListener('load', () => {
 
@@ -13,14 +10,38 @@ window.addEventListener('load', () => {
     const $messageInput = document.getElementById('messageInput');
     const $messageForm = document.getElementById('messageForm')
     const $messagesContainer = document.getElementById('messagesContainer')
+    const $canvas = document.getElementById('canvas')
+    const $canvasButtonsRow = document.getElementById('canvasButtonsRow')
     
-    // username is used to be compared with 'from' in 'msg' events
-    let username;
+    // Login
+    $loginForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        // Login with `name`
+        let name = $nameInput.value;
+        login(name)
+
+        // Remove the login form and show other UI components
+        $loginForm.remove()
+        $messageForm.classList.remove('hidden')
+        $canvas.classList.remove('hidden')
+        $canvasButtonsRow.classList.remove('hidden')
+    })
+
+    // Send Message
+    $messageForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        const message = $messageInput.value;
+        $messageInput.value = "";
+        // Send
+        socket.emit('msg', message)
+    })
+
+
     function login(name) {
-        // Create a socket connection
         username = name;
-        socket.emit('login', name)
-        // Recieve Message
+        socket.emit('login', username)
+        
+        // Recieve Messages
         socket.on('msg', (data) => {
             if (data.from != username) {
                 say(data.from, data.message)
@@ -29,32 +50,11 @@ window.addEventListener('load', () => {
             }
         })
     }
-
-    // Login
-    $loginForm.addEventListener('submit', function(event) {
-        event.preventDefault()
-        // Login with `name`
-        let name = $nameInput.value;
-        login(name)
-        // Remove the login form and
-        // show the chat message form
-        $loginForm.remove()
-        $messageForm.classList.remove('hidden')
-    })
-
-    // Send Message
-    $messageForm.addEventListener('submit', function(event) {
-        event.preventDefault()
-        let message = $messageInput.value;
-        $messageInput.value = "";
-        // Send
-        socket.emit('msg', message)
-    })
-
+    
     function say(name, message) {
         $messagesContainer.innerHTML +=
         `<div class="chat-message">
-            <span style="color: red; font-weight: bold;">${name}:</span> ${message}
+            <span style="color: red; font-weight: bold;">${name}:</span> <span class="msg">${message}</span>
         </div>`
         // Scroll down to last message
         $messagesContainer.scrollTop = $messagesContainer.scrollHeight
